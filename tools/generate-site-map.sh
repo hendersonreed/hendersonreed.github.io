916df80270a4d9b31ee4f1f2eb496be63ffb8b73
Henderson Reed Hummel <reed.hummel@gmail.com>
2024-04-17 13:48:16 -0700

    add sitemap class to sitemap text.

--- tools/generate-site-map.sh
+++ tools/generate-site-map.sh
@@ -39,5 +39,5 @@ tree \
     -o ../_sitemap.html
 cd -
 
-cat header.html <(echo "<h1>Sitemap</h1><br>") <(echo "<pre>") _sitemap.html <(echo "</pre>") footer.html > docs/sitemap.html
+cat header.html <(echo "<h1>Sitemap</h1><br>") <(echo "<pre class=\"sitemap\">") _sitemap.html <(echo "</pre>") footer.html > docs/sitemap.html
 rm _sitemap.html
