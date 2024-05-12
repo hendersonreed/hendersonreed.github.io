ad7ab3e20e28577709c0075031a44a94d803fe40
Henderson Reed Hummel <reed.hummel@gmail.com>
2024-05-12 11:13:54 -0400

    Better message on the publish script.

--- tools/publish.sh
+++ tools/publish.sh
@@ -1,10 +1,11 @@
 #!/bin/env bash
+set -euo pipefail
 
 # common stuff for building the site
 psg clean
 ./tools/heavy-lifting.sh
 
-echo "enter your commit message now: "
+echo "enter your commit message now: (ctrl-c to cancel)"
 read -r commit_msg
 
 git add .
