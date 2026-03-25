a0e607e645ac640020ef9e0478bd9890c40d3936
Henderson Reed Hummel <reed.hummel@gmail.com>
2026-03-25 03:55:25 -0700

    Fix styling for blogpost index.

--- tools/generate-blog-index.py
+++ tools/generate-blog-index.py
@@ -18,7 +18,7 @@ def generate_blog_index(atom_feed_path):
         link_element = entry.find('{http://www.w3.org/2005/Atom}link')
         link = link_element.get('href')  # Get the value of the 'href' attribute
         updated = entry.find('{http://www.w3.org/2005/Atom}updated').text.split("T")[0]
-        blog_index_html += f'<li>{updated} | <a href="{link}">{title}</a></li>\n'
+        blog_index_html += f'<li class="index-date">{updated} | <a href="{link}">{title}</a></li>\n'
     blog_index_html += "</ul>\n"
 
     return blog_index_html
